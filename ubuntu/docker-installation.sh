063b43a7aee586954ab1030eecf14b7522973009
dannypsnl <dannypsnl@gmail.com>
2019-10-17 17:47:35 +0800

    fix, no sudo

--- ubuntu/docker-installation.sh
+++ ubuntu/docker-installation.sh
@@ -10,5 +10,5 @@ add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
+apt-get update
+apt-get install -y docker-ce
