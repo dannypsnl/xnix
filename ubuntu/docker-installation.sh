58a04b7c31d5118ac367bc99265c8203b064803d
dannypsnl <dannypsnl@gmail.com>
2019-10-17 17:46:27 +0800

    docker installation on ubuntu

---
+++ ubuntu/docker-installation.sh
@@ -0,0 +1,14 @@
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
