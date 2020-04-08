ab33e90925d9d5fdf0f2ddf417e8cc7b3541d543
dannypsnl <dannypsnl@gmail.com>
2020-04-08 17:32:26 +0800

    remove duplicate installation

--- zsh-set/install.sh
+++ zsh-set/install.sh
@@ -1,4 +1,2 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
