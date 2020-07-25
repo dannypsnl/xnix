2e63935ace6c1ae5d1954f159821bfecfed33f88
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-25 23:58:01 +0800

    `git i` replaced `init` to use different default branch

--- git/git-alias-setting.sh
+++ git/git-alias-setting.sh
@@ -8,6 +8,7 @@ git config --global alias.df 'diff'
 git config --global alias.st 'status'
 git config --global alias.ad 'add'
 git config --global alias.rb 'rebase'
+git config --global alias.i '!git init && git symbolic-ref HEAD refs/heads/dev'
 
 git config --global color.ui true
 
