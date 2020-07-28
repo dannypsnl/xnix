a1e1efbef665cb8515fa7a9b3220ba6c5001ad4f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-28 13:11:49 +0800

    setup default branch

--- git/git-alias-setting.sh
+++ git/git-alias-setting.sh
@@ -8,7 +8,6 @@ git config --global alias.df 'diff'
 git config --global alias.st 'status'
 git config --global alias.ad 'add'
 git config --global alias.rb 'rebase'
-git config --global alias.i '!git init && git symbolic-ref HEAD refs/heads/dev'
 
 git config --global color.ui true
 
@@ -16,3 +15,4 @@ git config --global core.editor 'nvim'
 git config --global branch.autosetuprebase 'always'
 git config --global push.default 'current'
 git config --global rebase.autosquash true
+git config --global init.defaultBranch develop
