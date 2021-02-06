1b115a317be7610eb1d2f9a9ecaa12b588cc858e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-02-06 16:55:29 +0000

    adjust git config

--- home.nix
+++ home.nix
@@ -165,7 +165,12 @@
       rb = "rebase";
       tree = "log --graph --decorate --pretty=oneline --abbrev-commit";
     };
-    ignores = [ "*~" "*.swp" "*.o" "*.a" "*.so" "*.aux" "*.dylib" ];
+    ignores = [
+      "*~" "*.swp" "*.o" "*.a" "*.so" "*.aux" "*.dylib"
+      # editor/IDE
+      ".idea/" ".vscode/"
+      ".ccls-cache/"
+    ];
     extraConfig = {
       init.defaultBranch = "develop";
       core.editor = "nvim";
