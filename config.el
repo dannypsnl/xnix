fa319f8d86aeda733d506ce91aec22d04cbff48f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-02-08 21:13:42 +0000

    [emacs] new shortcut
    * cmd+t: new tab
    * cmd+w: close tab

--- config.el
+++ config.el
@@ -12,7 +12,11 @@
 ; font size: 1/10pt * :height
 (set-face-attribute 'default nil :height 150)
 
-; cmd+k for commit
+; cmd+k: commit
 (global-set-key (kbd "s-k") 'magit-commit)
-; cmd+shift+k for push
+; cmd+shift+k: push
 (global-set-key (kbd "s-K") 'magit-push)
+; cmd+t: new tab
+(global-set-key (kbd "s-t") 'tab-bar-new-tab)
+; cmd+w: close tab
+(global-set-key (kbd "s-w") 'tab-bar-close-tab)
