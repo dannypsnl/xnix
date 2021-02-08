7ddd6dc97ed7ef8d322830a61a441b7aea4a2f13
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-02-09 01:16:55 +0800

    [git] extract config

--- home.nix
+++ home.nix
@@ -53,7 +53,7 @@ in
 
   home.file.".emacs".text = builtins.readFile ./init.el;
   programs.emacs = import ./emacs.nix;
-  programs.neovim = (import ./neovim.nix){ pkgs = pkgs; };
+  programs.neovim = (import ./neovim.nix) pkgs;
 
   programs.fzf = {
     enable = true;
@@ -105,35 +105,5 @@ in
     gitProtocol = "ssh";
   };
 
-  programs.git = {
-    enable = true;
-    userName = "Lîm Tsú-thuàn";
-    userEmail = "dannypsnl@gmail.com";
-    aliases = {
-      co = "checkout";
-      cm = "commit";
-      br = "branch";
-      mg = "merge";
-      df = "diff";
-      st = "status";
-      ad = "add";
-      rb = "rebase";
-      tree = "log --graph --decorate --pretty=oneline --abbrev-commit";
-    };
-    ignores = [
-      "*~" "*.swp" "*.o" "*.a" "*.so" "*.aux" "*.dylib"
-      # editor/IDE
-      ".idea/" ".vscode/"
-      ".ccls-cache/"
-    ];
-    extraConfig = {
-      init.defaultBranch = "develop";
-      core.editor = "nvim";
-      color.ui = true;
-      branch.autosetuprebase = "always";
-      push.default = "current";
-      rebase.autosquash = true;
-      commit.template = machine.xnixPath + "/commit-template.txt";
-    };
-  };
+  programs.git = (import ./git.nix) machine;
 }
