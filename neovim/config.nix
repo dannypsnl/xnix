b86d0e7ef79493d968647d6b1e735db09f5f1974
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-02-10 05:58:35 +0000

    [neovim] oldbook/distilled
    close #5

--- neovim/config.nix
+++ neovim/config.nix
@@ -1,11 +1,36 @@
-pkgs: {
+pkgs:
+let
+  customPlugins = {
+    oldbook-vim = pkgs.vimUtils.buildVimPlugin {
+      name = "oldbook-vim";
+      src = pkgs.fetchFromGitHub {
+        owner = "karoliskoncevicius";
+        repo = "oldbook-vim";
+        rev = "master";
+        sha256 = "1jrnl2947kv7gxw6jhybjglxav3w9173lp3421jv59xmi5bw0a9x";
+      };
+    };
+    distilled-vim = pkgs.vimUtils.buildVimPlugin {
+      name = "distilled-vim";
+      src = pkgs.fetchFromGitHub {
+        owner = "karoliskoncevicius";
+        repo = "distilled-vim";
+        rev = "master";
+        sha256 = "182xcmlb10h611m0awrbj41pz5phn2smvclzn9rajzal4ihwlg2g";
+      };
+    };
+  };
+  p = pkgs.vimPlugins // customPlugins;
+in {
   enable = true;
   viAlias = true;
   vimAlias = true;
-  plugins = with pkgs.vimPlugins; [
+  plugins = with p; [
     nerdtree
     # theme
-    solarized # oldbook-vim distilled-vim
+    solarized
+    oldbook-vim
+    #distilled-vim
     ack-vim
     ctrlp
     coc-nvim
