7258d024049096e3731ee833a944b79a0932a762
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-02-19 14:35:17 +0800

    purescript seems like borken

--- home.nix
+++ home.nix
@@ -54,10 +54,6 @@ in {
       ## erlang/elixir
       erlang
       elixir
-      ## purescript
-      purescript
-      nodePackages.purescript-language-server
-      nodePackages.purty
       ## ocaml
       ocaml
       opam
