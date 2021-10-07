0a05d791ba322d23a390f05410e2ab44d65b4ee5
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-10-07 15:46:16 +0800

    [vscode] set formatter by languages

--- vscode/config.nix
+++ vscode/config.nix
@@ -2,13 +2,17 @@
   enable = isNixOS;
   package = pkgs.vscode;
   extensions = with pkgs.vscode-extensions;
-    [ WakaTime.vscode-wakatime ]
+    [ WakaTime.vscode-wakatime esbenp.prettier-vscode ]
     ++ lib.optionals isNixOS [ bbenoist.Nix ms-vsliveshare.vsliveshare ];
   userSettings = {
     "workbench.colorTheme" = "Visual Studio Light";
     "editor.fontSize" = 13;
-    "editor.defaultFormatter" = "JakeBecker.elixir-ls";
     "editor.formatOnSave" = true;
     "editor.fontFamily" = "'PragmataPro Mono Liga'";
+    "[elixir].editor.defaultFormatter" = "JakeBecker.elixir-ls";
+    "[javascript].editor.defaultFormatter" = "esbenp.prettier-vscode";
+    "[javascriptreact].editor.defaultFormatter" = "esbenp.prettier-vscode";
+    "[typescript].editor.defaultFormatter" = "esbenp.prettier-vscode";
+    "[typescriptreact].editor.defaultFormatter" = "esbenp.prettier-vscode";
   };
 }
