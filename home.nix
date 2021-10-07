4dbec9da2ec31d5f6de0c175cf49b4cda557be13
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-10-07 10:53:58 +0800

    record vscode setting

--- home.nix
+++ home.nix
@@ -88,6 +88,18 @@ in {
   programs.vscode = {
     enable = isNixOS;
     package = pkgs.vscode;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.Nix
+      WakaTime.vscode-wakatime
+      ms-vsliveshare.vsliveshare
+    ];
+    userSettings = {
+      "workbench.colorTheme" = "Visual Studio Light";
+      "editor.fontSize" = 13;
+      "editor.defaultFormatter" = "JakeBecker.elixir-ls";
+      "editor.formatOnSave" = true;
+      "editor.fontFamily" = "'PragmataPro Mono Liga'";
+    };
   };
 
   home.file.".emacs".text = builtins.readFile ./emacs/init.el;
