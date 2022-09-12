05f038fafe53b16e5e3613cbb44fdae482f97cd6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-09-12 09:48:58 +0800

    link dir, not file
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- install.sh
+++ install.sh
@@ -5,16 +5,13 @@ ln -s $(pwd)/macos-extra ${HOME}/.macos-extra
 ln -s $(pwd)/zshrc ${HOME}/.zshrc
 
 # neovim
-mkdir -p ${HOME}/.config/nvim
-ln -s $(pwd)/vimrc ${HOME}/.config/nvim/init.vim
+ln -s $(pwd)/nvim ${HOME}/.config/nvim
 
 # kitty
-ln -s $(pwd)/kitty/kitty.conf ${HOME}/.config/kitty/kitty.conf
+ln -s $(pwd)/kitty ${HOME}/.config/kitty
 
 # agda
-ln -s $(pwd)/.agda/libraries ${HOME}/.agda/libraries
-ln -s $(pwd)/.agda/defaults ${HOME}/.agda/defaults
+ln -s $(pwd)/agda ${HOME}/.agda
 
 # git
-ln -s $(pwd)/git/config ${HOME}/.config/git/config
-ln -s $(pwd)/git/ignore ${HOME}/.config/git/ignore
+ln -s $(pwd)/git ${HOME}/.config/git
