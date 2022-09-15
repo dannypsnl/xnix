21b1a57fdec0b92f2d3234c22251efa6311040af
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-09-15 14:00:01 +0800

    add <leader> + a for text searching

--- nvim/lua/files_setup.lua
+++ nvim/lua/files_setup.lua
@@ -3,3 +3,5 @@ local keymap = vim.keymap.set
 keymap('n', '<leader>f', '<cmd>NERDTreeToggle<cr>')
 -- searching file in project
 keymap('n', '<leader>.', '<cmd>GFiles<cr>')
+-- searching text in project
+keymap('n', '<leader>a', '<cmd>Ag<cr>')
