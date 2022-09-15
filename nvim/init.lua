56717ad82fcd8f0daae908210746c3de9bc69e0c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-09-15 13:01:28 +0800

    using lua to config most stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,50 +43,55 @@ require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip'
 end)
 
-vim.cmd [[
-let mapleader="\<Space>"
+local cmd = vim.cmd
 
-" Common useful vim configuration
-set nobackup
-set backspace=indent,eol,start
-set expandtab
-set tabstop=4
-retab
-set shiftwidth=4
-set undofile
-set cursorline
-set whichwrap+=<,>,[,]
-set hlsearch
-" show line number
-set number
-set nolist
-set listchars=tab:→\ ,trail:•
-set list
+vim.g.mapleader = ' ' -- use space as <leader>
+-- common useful configuration
+vim.o.backspace = 'indent,eol,start'
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+cmd [[ retab ]]
+vim.o.undofile = true
+vim.o.cursorline = true
+vim.o.whichwrap = vim.o.whichwrap .. '<,>,[,]'
+vim.o.hlsearch = true
+vim.o.backup = false -- no backup file
+vim.o.number = true -- show line number
+vim.o.list = false
+vim.o.listchars = 'tab:→ ,trail:•'
+vim.o.list = true
+vim.o.mouse = 'a' -- allow mouse action
+vim.o.virtualedit = 'all' -- allow virtual editing
+-- completion setup
+vim.o.completeopt = 'menu,menuone,noselect'
+-- ack
+vim.g.ackprg = 'ag --vimgrep'
 
-" open from last editing point
+-- open from last editing point
+cmd [[
 if has("autocmd")
   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
 endif
-" close whole editor when only NERDTree window leave
+]]
+-- close whole editor when only NERDTree window leave
+cmd [[
 autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree")
       \ && b:NERDTree.isTabTree()) | q | endif
+]]
 
-let g:ackprg = 'ag --vimgrep'
-
-" allow mouse action
-set mouse=a
-" allow virtual editing
-set virtualedit=all
-
-" Themes configuration
+-- Themes configuration
+cmd [[
 syntax enable
 "let g:solarized_termcolors=256
 "colorscheme solarized
 "colorscheme oldbook
 colorscheme distilled
 set termguicolors
+]]
 
-" Ignore up/low case and order! I'm typo king
+-- Ignore up/low case and order! I'm typo king
+cmd [[
 cab W w
 cab Q q
 cab Wq wq
@@ -96,9 +101,6 @@ cab qw wq
 cab QW wq
 cab Qw wq
 cab qW wq
-
-" completion setup
-set completeopt=menu,menuone,noselect
 ]]
 
 require("git")
