06ccea9f234038cb0c44b4753b253fade8c42795
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-01-12 12:24:43 +0800

    open orgmode in a floating window
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- nvim/lua/orgmode_setup.lua
+++ nvim/lua/orgmode_setup.lua
@@ -33,4 +33,25 @@ require('nvim-treesitter.configs').setup {
 require('orgmode').setup({
 	org_agenda_files = { '~/iCloud/org/*' },
 	org_default_notes_file = '~/iCloud/org/inbox.org',
+	win_split_mode = function(name)
+		local bufnr = vim.api.nvim_create_buf(false, true)
+		--- Setting buffer name is required
+		vim.api.nvim_buf_set_name(bufnr, name)
+
+		local fill = 0.8
+		local width = math.floor((vim.o.columns * fill))
+		local height = math.floor((vim.o.lines * fill))
+		local row = math.floor((((vim.o.lines - height) / 2) - 1))
+		local col = math.floor(((vim.o.columns - width) / 2))
+
+		vim.api.nvim_open_win(bufnr, true, {
+			relative = "editor",
+			width = width,
+			height = height,
+			row = row,
+			col = col,
+			style = "minimal",
+			border = "rounded"
+		})
+	end
 })
