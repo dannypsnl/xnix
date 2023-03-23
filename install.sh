b02e8956dbd047d0827f15be10647b8b829a2e27
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-03-23 16:38:43 +0800

    make install script more useful by default
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
+# install brew
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install neovim direnv
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install ghcup
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+# install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# config
+mkdir -p ${HOME}/.config
 # zsh
-touch ${HOME}/.macos-extra
 ln -s $(pwd)/zshrc ${HOME}/.zshrc
-
 # neovim
 ln -s $(pwd)/nvim ${HOME}/.config/nvim
-
 # kitty
 ln -s $(pwd)/kitty ${HOME}/.config/kitty
-
 # git
 ln -s $(pwd)/git ${HOME}/.config/git
