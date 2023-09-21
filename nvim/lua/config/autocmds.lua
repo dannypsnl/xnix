5d5365154012d122aaf6ac8d0ceadcf38f07b467
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2023-09-21 11:05:36 +0800

    remove no effect setup (this is default in lazy.nvim)
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,12 +7,6 @@ local cmd = vim.cmd
 cmd([[
 set noswapfile
 ]])
--- open from last editing point
-cmd([[
-if has("autocmd")
-  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-endif
-]])
 
 -- Ignore up/low case and order! I'm typo king
 cmd([[
